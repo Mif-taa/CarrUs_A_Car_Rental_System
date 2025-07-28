3fa0c3b52335c07ce5873ff6b5810dc03834baa3
Mif-taa <miftahuljannatrishta@gmail.com>
2025-07-28 12:37:24 +0600

    Add files via upload

---
+++ admin/frontend/src/App.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import AdminLogin from '../frontend/src/AdminLogin';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <AdminLogin />
+//     </div>
+//   );
+// }
+
+// export default App;
